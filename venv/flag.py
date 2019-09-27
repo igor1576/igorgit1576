73a77745447de82f5bfce0c86d06f8fa537649e9
Admin <igor@gymn1576.ru>
2019-09-27 21:35:13 +0300

    Нарисовали белую часть флага

--- venv/flag.py
+++ venv/flag.py
@@ -1,1 +1,31 @@
-print('123')
+import pygame
+
+pygame.init()
+
+# Считываем размер окна
+size = width, height = 300, 300
+screen = pygame.display.set_mode(size)
+
+
+def draw():
+     # Задаем параметры прямоугольника
+    rect_color = pygame.Color('white')
+    rect_width = 0
+    rect_point = [(0, 0), (300, 100)]
+    # Рисуем прямоугольник
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
+    rect_color = pygame.Color('black')
+    rect_width = 0
+    rect_point = [(0, 0), (10, 300)]
+     # Рисуем прямоугольник
+    pygame.draw.rect(screen, rect_color, rect_point, rect_width)
+
+
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
